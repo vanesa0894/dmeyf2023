0ec1ea1053d698086973d3cb30d4c08f8ba09c1d
vanesa0894 <vanesaflores0894@gmail.com>
2023-10-11 22:06:37 -0300

    drif

--- src/drifting/tratamiento_drifting.r
+++ src/drifting/tratamiento_drifting.r
@@ -10,6 +10,11 @@ setwd("C:/Users/vanes/Documents/UBA/2do_cuatrimestre/DMEyF")
 # cargo el dataset 
 dataset <- fread("./datasets/competencia_02.csv.gz", stringsAsFactors = TRUE)
 
+data_types <- sapply(dataset, class)
+
+# Muestra los tipos de datos
+print(data_types)
+
 # selecciono variables monetarias
 campos_a_tratar = c("mrentabilidad","mrentabilidad_annual","mcomisiones","mactivos_margen","mpasivos_margen","mcuenta_corriente_adicional","mcuenta_corriente","mcaja_ahorro","mcaja_ahorro_adicional","mcaja_ahorro_dolares","mcuentas_saldo","mautoservicio","mtarjeta_visa_consumo","mtarjeta_master_consumo","mprestamos_personales","mprestamos_prendarios","mprestamos_hipotecarios","mplazo_fijo_dolares","mplazo_fijo_pesos","minversion1_pesos","minversion1_dolares","minversion2","mpayroll","mpayroll2","mcuenta_debitos_automaticos","mttarjeta_master_debitos_automaticos","mpagodeservicios","mpagomiscuentas","mcajeros_propios_descuentos","mtarjeta_visa_descuentos","mtarjeta_master_descuentos","mcomisiones_mantenimiento","mcomisiones_otras","mforex_buy","mforex_sell","mtransferencias_recibidas","mtransferencias_emitidas","mextraccion_autoservicio","mcheques_depositados","mcheques_emitidos","mcheques_depositados_rechazados","mcheques_emitidos_rechazados","matm","matm_other","Master_mfinanciacion_limite","Master_msaldototal","Master_msaldopesos","Master_msaldodolares","Master_mconsumospesos","Master_mconsumosdolares","Master_mlimitecompra","Master_madelantopesos","Master_madelantodolares","Master_mpagado","Master_mpagospesos","Master_mpagosdolares","Master_mconsumototal","Master_mpagominimo","Visa_mfinanciacion_limite",
 "Visa_msaldototal","Visa_msaldopesos","Visa_msaldodolares","Visa_mconsumospesos","Visa_mconsumosdolares","Visa_mlimitecompra",
@@ -25,6 +30,11 @@ dataset[, (paste0(campos_a_tratar, "_rank")) := lapply(.SD, function(x) frankv(x
 dataset[, (campos_a_tratar) := NULL]
 
 con <- gzfile("./datasets/competencia_02_dd.csv.gz", "w")
+
+data_types <- sapply(dataset, class)
+
+# Muestra los tipos de datos
+print(data_types)
 
 # Escribe el dataframe en el archivo GZIP como CSV
 write.csv(dataset, con, row.names = FALSE)
