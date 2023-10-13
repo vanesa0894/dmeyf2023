9a6d76fbed94bad38aa66c659755a58435038326
vanesa0894 <vanesaflores0894@gmail.com>
2023-10-13 12:19:53 -0300

    BO eterna 2 mejor ganancia 880007 conf

--- src/lightgbm/z524_lightgbm_final.r
+++ src/lightgbm/z524_lightgbm_final.r
@@ -14,21 +14,21 @@ require("lightgbm")
 # defino los parametros de la corrida, en una lista, la variable global  PARAM
 #  muy pronto esto se leera desde un archivo formato .yaml
 PARAM <- list()
-PARAM$experimento <- "KA5240_11"
+PARAM$experimento <- "KA5240_12"
 
 PARAM$input$dataset <- "./datasets/competencia_02_fe_202012.csv.gz"
 
 # meses donde se entrena el modelo
 PARAM$input$training <- c(202012, 202101, 202102, 202103, 202104, 202105)
 PARAM$input$future <- c(202107) # meses donde se aplica el modelo
-# Tomo parámetros obtenidos de BO de casi 5 días (1 mejor ganancia)
+# Tomo parámetros obtenidos de BO de casi 5 días (2 mejor ganancia)
 PARAM$finalmodel$semilla <- 880007
 
-PARAM$finalmodel$num_iterations <- 5968
-PARAM$finalmodel$learning_rate <- 0.0167320818655866
-PARAM$finalmodel$feature_fraction <- 0.2033037718958
-PARAM$finalmodel$min_data_in_leaf <- 31
-PARAM$finalmodel$num_leaves <- 631
+PARAM$finalmodel$num_iterations <- 6875
+PARAM$finalmodel$learning_rate <- 0.0133326486877864
+PARAM$finalmodel$feature_fraction <- 0.216652156817687
+PARAM$finalmodel$min_data_in_leaf <- 516
+PARAM$finalmodel$num_leaves <- 685
 
 
 PARAM$finalmodel$max_bin <- 31
